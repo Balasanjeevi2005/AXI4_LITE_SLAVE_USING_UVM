0d48e8bf6d515a29cdc2811d8f2188a745c5c399
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-25 13:35:52 +0530

    Add test_pkg package with UVM components

---
+++ src/TB/test_pkg.sv
@@ -0,0 +1,15 @@
+package test_pkg;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "axi4_if.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "inp_drv.sv"
+`include "inp_mon.sv"
+`include "out_mon.sv"
+`include "inp_agnt.sv"
+`include "out_agnt.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "test.sv"
+endpackage 
