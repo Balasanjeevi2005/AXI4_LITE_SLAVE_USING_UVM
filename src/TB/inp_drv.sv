30cd181de0ba07ef254164c82bd9f07fbaf07829
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-27 00:01:25 +0530

    updated

--- src/TB/inp_drv.sv
+++ src/TB/inp_drv.sv
@@ -3,7 +3,9 @@ class inp_drv extends uvm_driver#(trans);
   `uvm_component_utils(inp_drv)
   
   axi4_cfg c_h;
-  virtual avi4_if.IN_DRV vif;
+  virtual axi4_if.IN_DRV vif;
+  bit a_flag=1'b0;
+  bit d_flag=1'b0;
   
   function new(string name="inp_drv",uvm_component parent);
     super.new(name,parent);
@@ -11,7 +13,7 @@ class inp_drv extends uvm_driver#(trans);
   
   function void built_phase(uvm_phase phase);
     super.built_phase(phase);
-    if(!uvm_config_db#(axi4_h)::get(this,"","axi4_cfg",c_h))
+    if(!uvm_config_db#(axi4_cfg)::get(this,"","axi4_cfg",c_h))
       `uvm_fatal("config fail","-------------->inp_drv config fail");
   endfunction
   
@@ -22,41 +24,51 @@ class inp_drv extends uvm_driver#(trans);
   
   task run_phase(uvm_phase phase);
     
-    @(vif.inp_mon_cb);
-    vif.inp_drv_cb.ARESETn <= 1'b0;
-    @(vif.inp_mon_cb);
-    vif.inp_drv_cb.ARESETn <= 1'b1;
-    @(vif.inp_mon_cb);
-    vif.inp_drv_cb.ARESETn <= 1'b0;
-    @(vif.inp_mon_cb);
-    vif.inp_drv_cb.ARESETn <= 1'b1;
-    
     forever begin
       seq_item_port.get_next_item(req);
       drive(req);
+      `uvm_info("inp_drv",$sformatf("input driver:\n%s",req.sprint()),UVM_NONE);
       seq_item_port.item_done();
     end
     
   endtask
   
   task drive(trans t);
+ 
+    @(vif.inp_drv_cb);
     
-    `uvm_info("inp_drv",$sformatf("input driver:\n%s",t.sprint()),UVM_NONE);
-    
-    vif.inp_drv_cb.AWADDR <= t.AWADDR;
-    vif.inp_drv_cb.AWVALID <= t.AWVALID;
-    vif.inp_drv_cb.WDATA <= t.WDATA;
-    vif.inp_drv_cb.WSTRB <= t.WSTRB;
-    vif.inp_drv_cb.WVALID <= t.WVALID;
-    vif.inp_drv_cb.BREADY <= t.BREADY;
-    vif.inp_drv_cb.ARADDR <= t.ARADDR;
-    vif.inp_drv_cb.ARVALID <= t.ARVALID;
-    vif.inp_drv_cb.RREADY <= t.RREADY;
-
-    vif.inp_drv_cb.AWPROT <= t.AWPROT;
-    vif.inp_drv_cb.ARPROT <= t.ARPROT;
-    
-    
+    if(t.AWVALID || t.WVALID)begin
+      
+      if(t.AWVALID && (!a_flag))begin
+        vif.inp_drv_cb.AWADDR  <= t.AWADDR;
+        vif.inp_drv_cb.AWVALID <= t.AWVALID;
+        a_flag=1'b1;
+      end
+      
+      if(t.WVALID && (!d_flag)) begin
+        vif.inp_drv_cb.WDATA   <= t.WDATA;
+        vif.inp_drv_cb.WSTRB   <= t.WSTRB;
+        vif.inp_drv_cb.WVALID  <= t.WVALID;
+        d_flag=1'b1;
+      end
+      
+      if(t.BREADY && vif.inp_drv_cb.BVALID)begin
+        vif.inp_drv_cb.BREADY  <= t.BREADY;
+        a_flag=1'b0;
+        d_flag=1'b0;
+      end
+        
+    end
+    if (t.ARVALID) begin
+      vif.inp_drv_cb.ARADDR  <= t.ARADDR;
+      vif.inp_drv_cb.ARVALID <= t.ARVALID;
+      
+      while(!(t.RREADY && vif.inp_drv_cb.RVALID))begin
+        @(vif.inp_drv_cb);
+      end
+      
+      vif.inp_drv_cb.RREADY  <= t.RREADY;
+    end
   endtask
   
 endclass
