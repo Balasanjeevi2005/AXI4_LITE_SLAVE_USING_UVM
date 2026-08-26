11a2715389c69207e98d88f0f9ab402e03c4eadb
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-27 00:44:05 +0530

    updated

--- src/TB/inp_mon.sv
+++ src/TB/inp_mon.sv
@@ -25,7 +25,6 @@ class inp_mon extends uvm_monitor;
   endfunction
   
   task run_phase(uvm_phase phase);
-    t=trans::type_id::create("t");
     forever begin
       collect_inp();
       `uvm_info("INP_MON",$sformatf("INP_MON:\n%S",t.sprint()),UVM_LOW);
@@ -36,18 +35,20 @@ class inp_mon extends uvm_monitor;
     
     repeat(2)@(vif.inp_mon_cb);
     
-    t.AWADDR  <= vif.inp_mon_cb.AWADDR;
-    t.AWVALID <= vif.inp_mon_cb.AWVALID;
-    t.WDATA   <= vif.inp_mon_cb.WDATA;
-    t.WSTRB   <= vif.inp_mon_cb.WSTRB;
-    t.WVALID  <= vif.inp_mon_cb.WVALID;
-    t.BREADY  <= vif.inp_mon_cb.BREADY;
-    t.ARADDR  <= vif.inp_mon_cb.ARADDR;
-    t.ARVALID <= vif.inp_mon_cb.ARVALID;
-    t.RREADY  <= vif.inp_mon_cb.RREADY;
+    t=trans::type_id::create("t");
     
-   // t.AWPROT  <= vif.inp_mon_cb.AWPROT;
-   // t.ARPROT  <= vif.inp_mon_cb.ARPROT;
+    t.AWADDR  = vif.inp_mon_cb.AWADDR;
+    t.AWVALID = vif.inp_mon_cb.AWVALID;
+    t.WDATA   = vif.inp_mon_cb.WDATA;
+    t.WSTRB   = vif.inp_mon_cb.WSTRB;
+    t.WVALID  = vif.inp_mon_cb.WVALID;
+    t.BREADY  = vif.inp_mon_cb.BREADY;
+    t.ARADDR  = vif.inp_mon_cb.ARADDR;
+    t.ARVALID = vif.inp_mon_cb.ARVALID;
+    t.RREADY  = vif.inp_mon_cb.RREADY;
+    
+   // t.AWPROT  = vif.inp_mon_cb.AWPROT;
+   // t.ARPROT  = vif.inp_mon_cb.ARPROT;
     
     inp_mon_port.write(t);
   endtask
