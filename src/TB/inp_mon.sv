7afe8fdcca01f5bdd457f5bdceea7ba5e99dcfdb
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-27 00:02:00 +0530

    updated
    Reduced the number of repetitions in collect_inp task and updated signal assignments to non-blocking assignments.

--- src/TB/inp_mon.sv
+++ src/TB/inp_mon.sv
@@ -1,9 +1,12 @@
 class inp_mon extends uvm_monitor;
   
   `uvm_component_utils(inp_mon)
+  uvm_analysis_port #(trans) inp_mon_port;
   
   axi4_if.IN_MON vif;
   axi4_cfg c_h;
+  
+  trans t;
   
   function new(string name="inp_mon",uvm_component parent);
     super.new(name,parent);
@@ -16,7 +19,7 @@ class inp_mon extends uvm_monitor;
     inp_mon_port=new("inp_mon_port",this);
   endfunction
   
-  function void connect_phase(uvm_phase phase)
+  function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     vif=c_h.vif;
   endfunction
@@ -31,20 +34,20 @@ class inp_mon extends uvm_monitor;
   
   virtual task collect_inp();
     
-    repeat(3)@(vif.inp_mon_cb);
+    repeat(2)@(vif.inp_mon_cb);
     
-    t.AWADDR  = vif.inp_mon_cb.AWADDR;
-    t.AWVALID = vif.inp_mon_cb.AWVALID;
-    t.WDATA   = vif.inp_mon_cb.WDATA;
-    t.WSTRB   = vif.inp_mon_cb.WSTRB;
-    t.WVALID  = vif.inp_mon_cb.WVALID;
-    t.BREADY  = vif.inp_mon_cb.BREADY;
-    t.ARADDR  = vif.inp_mon_cb.ARADDR;
-    t.ARVALID = vif.inp_mon_cb.ARVALID;
-    t.RREADY  = vif.inp_mon_cb.RREADY;
+    t.AWADDR  <= vif.inp_mon_cb.AWADDR;
+    t.AWVALID <= vif.inp_mon_cb.AWVALID;
+    t.WDATA   <= vif.inp_mon_cb.WDATA;
+    t.WSTRB   <= vif.inp_mon_cb.WSTRB;
+    t.WVALID  <= vif.inp_mon_cb.WVALID;
+    t.BREADY  <= vif.inp_mon_cb.BREADY;
+    t.ARADDR  <= vif.inp_mon_cb.ARADDR;
+    t.ARVALID <= vif.inp_mon_cb.ARVALID;
+    t.RREADY  <= vif.inp_mon_cb.RREADY;
     
-    t.AWPROT  = vif.inp_mon_cb.AWPROT;
-    t.ARPROT  = vif.inp_mon_cb.ARPROT;
+   // t.AWPROT  <= vif.inp_mon_cb.AWPROT;
+   // t.ARPROT  <= vif.inp_mon_cb.ARPROT;
     
     inp_mon_port.write(t);
   endtask
