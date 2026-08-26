aad556abe4e254b88df3c9210e6eedd19bd15e3d
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-27 00:00:21 +0530

    updated

--- src/TB/env.sv
+++ src/TB/env.sv
@@ -4,7 +4,8 @@ class env extends uvm_env;
  inp_agnt inp_agt_h;
  out_agnt out_agt_h;
  scoreboard sb_h;
-
+ subscriber sub_h;
+  
  axi4_cfg m_cfg;
 
  function new(string name="env",uvm_component parent);
@@ -20,13 +21,19 @@ class env extends uvm_env;
   inp_agt_h=inp_agnt::type_id::create("inp_agt_h",this);
   out_agt_h=out_agnt::type_id::create("out_agt_h",this);
   sb_h=scoreboard::type_id::create("sb_h",this);
+  sub_h = subscriber::type_id::create("sub_h", this);
 
  endfunction
 
  function void connect_phase(uvm_phase phase);
-	super.connect_phase(phase);
-	inp_agt_h.im_h.inp_mon_port.connect(sb_h.inp_mon_fifo.analysis_export);
-	out_agt_h.om_h.out_mon_port.connect(sb_h.out_mon_fifo.analysis_export);
+   
+   super.connect_phase(phase);
+   
+   inp_agt_h.im_h.inp_mon_port.connect(sb_h.inp_mon_fifo.analysis_export);
+   out_agt_h.om_h.out_mon_port.connect(sb_h.out_mon_fifo.analysis_export);
+   
+   inp_agt_h.im_h.inp_mon_port.connect(sub_h.analysis_export);
+   
  endfunction
 
 endclass
