fe105bf1df1ca7028ba61676cea47cf5f2c2efe0
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-28 07:12:57 +0530

    REMOVEDD THE WAITING

--- src/TB/inp_drv.sv
+++ src/TB/inp_drv.sv
@@ -5,8 +5,6 @@ class inp_drv extends uvm_driver#(trans);
   axi4_cfg c_h;
   virtual axi4_if.IN_DRV vif;
   trans t;
-  //bit a_flag=1'b0;
-  //bit d_flag=1'b0;
   
   function new(string name="inp_drv",uvm_component parent);
     super.new(name,parent);
@@ -28,7 +26,7 @@ class inp_drv extends uvm_driver#(trans);
     forever begin
       seq_item_port.get_next_item(req);
       drive(req);
-      `uvm_info("inp_drv",$sformatf("input driver:\n%s",req.sprint()),UVM_NONE);
+      `uvm_info("inp_drv",$sformatf("input driver:\n%s",req.sprint()),UVM_MEDIUM);
       seq_item_port.item_done();
     end
     
@@ -37,36 +35,16 @@ class inp_drv extends uvm_driver#(trans);
   task drive(trans t);
  
     @(vif.inp_drv_cb);
-    
-    if(t.AWVALID || t.WVALID)begin
-      
-      if(t.AWVALID)begin// && (!a_flag)
-        vif.inp_drv_cb.AWADDR  <= t.AWADDR;
-        vif.inp_drv_cb.AWVALID <= t.AWVALID;
-        //a_flag=1'b1;
-      end
-      
-      if(t.WVALID) begin//&& (!d_flag)
-        vif.inp_drv_cb.WDATA   <= t.WDATA;
-        vif.inp_drv_cb.WSTRB   <= t.WSTRB;
-        vif.inp_drv_cb.WVALID  <= t.WVALID;
-        //d_flag=1'b1;
-      end
-      
-      if(t.BREADY && vif.inp_drv_cb.BVALID)begin
-        vif.inp_drv_cb.BREADY  <= t.BREADY;
-        //a_flag=1'b0;
-        //d_flag=1'b0;
-      end
-        
-    end
-    if (t.ARVALID) begin
-      vif.inp_drv_cb.ARADDR  <= t.ARADDR;
-      vif.inp_drv_cb.ARVALID <= t.ARVALID;
-      
-      if(t.RREADY && vif.inp_drv_cb.RVALID)
-        vif.inp_drv_cb.RREADY  <= t.RREADY;
-    end
+    vif.inp_drv_cb.AWADDR  <= t.AWADDR;
+    vif.inp_drv_cb.AWVALID <= t.AWVALID;
+    vif.inp_drv_cb.WDATA   <= t.WDATA;
+    vif.inp_drv_cb.WSTRB   <= t.WSTRB;
+    vif.inp_drv_cb.WVALID  <= t.WVALID;
+    vif.inp_drv_cb.BREADY  <= t.BREADY;
+    vif.inp_drv_cb.ARADDR  <= t.ARADDR;
+    vif.inp_drv_cb.ARVALID <= t.ARVALID;
+    vif.inp_drv_cb.RREADY  <= t.RREADY;
+
   endtask
   
 endclass
