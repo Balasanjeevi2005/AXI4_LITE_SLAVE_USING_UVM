ea23a4725fcf15f390620b8894c98168b6cfab4b
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:38:32 +0530

    updated

--- src/TB/test.sv
+++ src/TB/test.sv
@@ -5,7 +5,7 @@ class test extends uvm_test;
   env e_h;
   axi4_cfg c_h;
   
-  function new(string="test",uvm_component parent);
+  function new(string name="test",uvm_component parent);
     super.new(name,parent);
   endfunction
   
@@ -15,10 +15,10 @@ class test extends uvm_test;
     c_h=axi4_cfg::type_id::create("c_h");
     
     if(!uvm_config_db#(virtual axi4_if)::get(this,"","axi4_if",c_h.vif))
-      `uvm_fata("config fail","-------------->test virtual_interface config fail");
+      `uvm_fatal("config fail","-------------->test virtual_interface config fail");
     
-    c_h.inp_agent_is_active==UVM_ACTIVE;
-    c_h.out_agent_is_passive==UVM_PASSIVE;
+    c_h.inp_agnt_is_active=UVM_ACTIVE;
+    c_h.out_agnt_is_passive=UVM_PASSIVE;
     
     uvm_config_db#(axi4_cfg)::set(this,"*","axi4_cfg",c_h);
     
@@ -38,34 +38,52 @@ class test1 extends test;
   
   `uvm_component_utils(test1)
   
-  wr_seq w1;
+  w_seq w1;
   r_seq r1;
+  wr_seq wr1;
+
   err_seq e1;
   direct_seq d1;
+  fsm_seq f1;
   
-  function new(string="test1",uvm_component parent);
-    super.new(name,parent);
+  function new(string name="test1",uvm_component parent);
+    super.new(name,parent); 
   endfunction
   
- // function void build_phase(uvm_phase phase);
-	//super.build_phase(phase);
-  //endfunction
+  function void build_phase(uvm_phase phase);
+	super.build_phase(phase);
+  endfunction
   
   task run_phase(uvm_phase phase);
     
     phase.raise_objection(this);
-    
-    w1=wr_seq::type_id::create("w1");
-    r1=wr_seq::type_id::create("r1");
-    e1=wr_seq::type_id::create("e1");
-    d1=wr_seq::type_id::create("d1");
-    
-    w1.start(e_h.inp_agnt_h.sr_h);
-    r1.start(e_h.inp_agnt_h.sr_h);
-    e1.start(e_h.inp_agnt_h.sr_h);
-    c1.start(e_h.inp_agnt_h.sr_h);
-
-	#50;  
+    fork 
+      begin
+        w1=w_seq::type_id::create("w1");
+        w1.start(e_h.inp_agnt_h.sr_h);
+      end
+      begin
+        r1=r_seq::type_id::create("r1");
+        r1.start(e_h.inp_agnt_h.sr_h);
+      end
+      begin
+        wr1=wr_seq::type_id::create("wr1");
+        wr1.start(e_h.inp_agnt_h.sr_h);
+      end
+      begin
+        e1=err_seq::type_id::create("e1");
+        e1.start(e_h.inp_agnt_h.sr_h);
+      end
+      begin
+        d1=direct_seq::type_id::create("d1");
+        d1.start(e_h.inp_agnt_h.sr_h);
+      end
+      begin
+       f1=fsm_seq::type_id::create("f1");
+       f1.start(e_h.inp_agnt_h.sr_h);
+      end
+    join
+    phase.phase_done.set_drain_time(this,20);
     phase.drop_objection(this);
     
   endtask
