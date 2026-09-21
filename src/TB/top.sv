65b48bee9b134be30871385948305a8f17d44432
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:38:53 +0530

    updatedd

--- src/TB/top.sv
+++ src/TB/top.sv
@@ -3,6 +3,7 @@
 `include "axi4_if.sv"
 `include "axi4_rtl.sv"
 `include "test_pkg.sv"
+
 import uvm_pkg::*;
 import test_pkg::*;
 
@@ -13,40 +14,52 @@ import test_pkg::*;
 
   //instatiate DUV
    axi4_lite_slave DUV(.ACLK(ACLK),
-                       .ARESETN(ARESETn),
+                       .ARESETn(ARESETn),
+
                        .AWADDR(DUV_IF.AWADDR),
                        .AWPROT(DUV_IF.AWPROT),
                        .AWVALID(DUV_IF.AWVALID),
+
                        .AWREADY(DUV_IF.AWREADY),
                        .WDATA(DUV_IF.WDATA),
                        .WSTRB(DUV_IF.WSTRB),
                        .WVALID(DUV_IF.WVALID),
                        .WREADY(DUV_IF.WREADY),
+
                        .BRESP(DUV_IF.BRESP),
                        .BVALID(DUV_IF.BVALID),
                        .BREADY(DUV_IF.BREADY),
+
                        .ARADDR(DUV_IF.ARADDR),
                        .ARPROT(DUV_IF.ARPROT),
                        .ARVALID(DUV_IF.ARVALID),
                        .ARREADY(DUV_IF.ARREADY),
+
                        .RDATA(DUV_IF.RDATA),
                        .RRESP(DUV_IF.RRESP),
                        .RVALID(DUV_IF.RVALID),
                        .RREADY(DUV_IF.RREADY));
+
+  //assign DUV_IF.w_state = DUV.wr_state;
+  //assign DUV_IF.r_state = DUV.rd_state;
    
    initial begin
      uvm_config_db#(virtual axi4_if)::set(null,"*","axi4_if",DUV_IF);
-     $dumpfile("waves.fsdb");
-     $dumpvars;
+     //$dumpfile("waves.fsdb");
+     //$dumpvars;
      run_test();	
    end
    
    initial begin
-     PRESETn=0;
-     #2 PRESETn=1;
-     #10 PRESETn=0;
-     repeat(5)@(posedge ACLK);
-     PRESETn=1;
+     ARESETn=0;
+     #2 ARESETn=1;
+     #10 ARESETn=0;
+     repeat(2)@(posedge ACLK);
+     ARESETn=1;
+     repeat(100)@(posedge ACLK);
+     ARESETn=0;
+     repeat(10)@(posedge ACLK);
+     ARESETn=1;
    end
    
    initial begin
