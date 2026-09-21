0a39e10d37de80e4d490fcbe6bee857893dabd8b
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:31:52 +0530

    updated
    Removed unused state variables and simplified clocking blocks. Updated input and output declarations to use single bit types instead of arrays.

--- src/TB/axi4_if.sv
+++ src/TB/axi4_if.sv
@@ -1,4 +1,6 @@
+`include "uvm_macros.svh"
 interface axi4_if(input logic ACLK,input logic ARESETn);
+  import uvm_pkg::*;
   logic[`ADDR_WIDTH-1:0]AWADDR;
   logic[2:0]AWPROT;
   logic AWVALID;
@@ -23,54 +25,59 @@ interface axi4_if(input logic ACLK,input logic ARESETn);
   logic RVALID;
   logic RREADY;
   
-  bit[2:0]w_state;
-  bit r_state;
-  
   clocking inp_drv_cb@(posedge ACLK);
-    
     default input #1 output #1;
-    
-    input BVALID;
-    input RVALID;
-    
-    output ARESETn;
 
-    output [`ADDR_WIDTH-1:0]AWADDR;
-    output [2:0]AWPROT;
+    input AWREADY;
+
+    input WREADY;
+ 
+    
+    input BRESP;
+    input BVALID;
+
+    input ARREADY;
+    
+    input RRESP;
+    input RDATA;
+    input RVALID;
+
+    output AWADDR;
+    output AWPROT;
     output AWVALID;
     
-    output[`DATA_WIDTH-1:0]WDATA;
-    output[(`DATA_WIDTH/8)-1:0]WSTRB;
+    output WDATA;
+    output WSTRB;
     output WVALID;
+    
     
     output BREADY;
 
-    output[`ADDR_WIDTH-1:0]ARADDR;
-    output[2:0]ARPROT;
+    output ARADDR;
+    output ARPROT;
     output ARVALID;
     
     output RREADY;
-    
   endclocking
   
   
   clocking inp_mon_cb@(posedge ACLK);
     default input #1 output #1;
+    
     input ARESETn;
 
-    input [`ADDR_WIDTH-1:0]AWADDR;
-    input [2:0]AWPROT;
+    input AWADDR;
+    input AWPROT;
     input AWVALID;
     
-    input[`DATA_WIDTH-1:0]WDATA;
-    input[(`DATA_WIDTH/8)-1:0]WSTRB;
+    input WDATA;
+    input WSTRB;
     input WVALID;
-    
     
     input BREADY;
 
-    input[`ADDR_WIDTH-1:0]ARADDR;
-    input[2:0]ARPROT;
+    input ARADDR;
+    input ARPROT;
     input ARVALID;
     
     input RREADY;
@@ -78,83 +85,117 @@ interface axi4_if(input logic ACLK,input logic ARESETn);
   
   clocking out_mon_cb@(posedge ACLK);
     default input #1 output #1;
+
     input AWREADY;
     input WREADY;
-    input[1:0]BRESP;
+
+    input BRESP;
     input BVALID;
+
     input ARREADY;
-    input[`DATA_WIDTH-1:0]RDATA;
-    input[1:0]RRESP;
+
+    input RDATA;
+    input RRESP;
     input RVALID;
+
   endclocking
   
   modport IN_DRV(clocking inp_drv_cb);
   modport IN_MON(clocking inp_mon_cb);
   modport OUT_MON(clocking out_mon_cb);
-  
-   widle_wboth:assert property(
+/*
+   // FSM signals coming from DUT
+  logic [2:0] wr_state;
+  logic       rd_state;
+    
+  localparam [2:0]
+      w_idle = 3'd0,
+      w_both = 3'd1,
+      w_addr = 3'd2,
+      w_data = 3'd3,
+      w_resp = 3'd4;
+
+  localparam
+      r_idle = 1'd0,
+      r_data = 1'd1;
+    
+  check_rst:assert property(
+    @(posedge ACLK)
+    !ARESETn|->(!AWREADY && !WREADY && !BVALID && !ARREADY && !RVALID)
+    )
+    else
+      `uvm_error("assertion_fail","reset failed"); 
+    
+     
+  widle_wboth:assert property(
      @(posedge ACLK)
-     (w_state==w_idle && PRESETn)|=> (w_state==w_both)
+     (w_state==w_idle && ARESETn)|=> (w_state==w_both)
     )
     else
       `uvm_error("assertion_fail","w_idle->w_both");
      
    wboth_wresp:assert property(
      @(posedge ACLK)
-     (AWVALID && WVALID)|=> (w_state==w_resp)
+     disable iff(!ARESETn)
+     ((w_state==w_both) && AWVALID && WVALID)|=> (w_state==w_resp)
     )
     else
       `uvm_error("assertion_fail","w_both->w_resp");
      
    wboth_waddr:assert property(
      @(posedge ACLK)
-     (AWVALID && !WVALID)|=> (w_state==w_addr)
+     disable iff(!ARESETn)
+     ((w_state==w_both) && AWVALID && !WVALID)|=> (w_state==w_addr)
     )
     else
       `uvm_error("assertion_fail","w_both->w_addr");
      
    wboth_wdata:assert property(
      @(posedge ACLK)
-     (!AWVALID && WVALID)|=> (w_state==w_data)
+     disable iff(!ARESETn)
+     ((!AWVALID && WVALID) && (w_state==w_both))|=> (w_state==w_data)
     )
     else
       `uvm_error("assertion_fail","w_both->w_data");
      
    waddr_wresp:assert property(
      @(posedge ACLK)
-     WVALID |=> (w_state==w_resp)
+     disable iff(!ARESETn)
+     (WVALID  && WREADY && (w_state==w_addr)) |=> (w_state==w_resp)
     )
     else
       `uvm_error("assertion_fail","w_addr->w_resp");
      
    wdata_wresp:assert property(
      @(posedge ACLK)
-     AWVALID |=> (w_state==w_resp)
+     disable iff(!ARESETn)
+     (AWVALID  && AWREADY && (w_state==w_data) ) |=> (w_state==w_resp)
     )
     else
       `uvm_error("assertion_fail","w_data->w_resp");
     
    wresp_widle:assert property(
      @(posedge ACLK)
-     (state==w_resp && BREADY) |=> (w_state==w_idle)
+     disable iff(!ARESETn)
+     ((w_state==w_resp) && BVALID && BREADY) |=> (w_state==w_idle)
     )
     else
       `uvm_error("assertion_fail","w_resp->w_idle");
     
    ridle_rdata:assert property(
      @(posedge ACLK)
-     (state==r_idle && PRESETn) |=> (r_state==r_data)
+     (r_state==r_idle && ARESETn) |=> (r_state==r_data)
     )
     else
       `uvm_error("assertion_fail","r_idle->r_data");
    
    rdata_ridle:assert property(
      @(posedge ACLK)
-     (state==r_data && RREADY) |=> (r_state==r_idle)
+     disable iff(!ARESETn)
+     (r_state==r_data && RVALID && RREADY) |=> (r_state==r_idle)
     )
     else
       `uvm_error("assertion_fail","r_data->r_idle");
       
-     
-   
+    */
 endinterface
