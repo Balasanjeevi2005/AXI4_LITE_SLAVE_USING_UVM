d76c0b1cc10f767c122d888d8bf241d3398ef7a2
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:34:21 +0530

    updated

--- src/TB/inp_mon.sv
+++ src/TB/inp_mon.sv
@@ -1,19 +1,20 @@
 class inp_mon extends uvm_monitor;
   
   `uvm_component_utils(inp_mon)
+  
   uvm_analysis_port #(trans) inp_mon_port;
   
-  axi4_if.IN_MON vif;
+  virtual axi4_if.IN_MON vif;
   axi4_cfg c_h;
-  
+
   trans t;
   
   function new(string name="inp_mon",uvm_component parent);
     super.new(name,parent);
   endfunction
   
-  function void built_phase(uvm_phase phase);
-    super.built_phase(phase);
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
     if(!uvm_config_db#(axi4_cfg)::get(this,"","axi4_cfg",c_h))
       `uvm_fatal("config fail","-------------->inp_mon config fail");
     inp_mon_port=new("inp_mon_port",this);
@@ -25,32 +26,38 @@ class inp_mon extends uvm_monitor;
   endfunction
   
   task run_phase(uvm_phase phase);
+    @(vif.inp_mon_cb);
     forever begin
       collect_inp();
-      `uvm_info("INP_MON",$sformatf("INP_MON:\n%S",t.sprint()),UVM_FULL);
+      `uvm_info("INP_MON",$sformatf("INP_MON:\n%s",t.sprint()),UVM_FULL);
     end
   endtask
   
   virtual task collect_inp();
     
-    repeat(2)@(vif.inp_mon_cb);
-    
+    repeat(1)@(vif.inp_mon_cb);
     t=trans::type_id::create("t");
-    
+    t.ARESETn = vif.inp_mon_cb.ARESETn;
+
     t.AWADDR  = vif.inp_mon_cb.AWADDR;
     t.AWVALID = vif.inp_mon_cb.AWVALID;
+    t.AWPROT  = vif.inp_mon_cb.AWPROT;
+      
     t.WDATA   = vif.inp_mon_cb.WDATA;
     t.WSTRB   = vif.inp_mon_cb.WSTRB;
     t.WVALID  = vif.inp_mon_cb.WVALID;
+      
     t.BREADY  = vif.inp_mon_cb.BREADY;
+      
     t.ARADDR  = vif.inp_mon_cb.ARADDR;
     t.ARVALID = vif.inp_mon_cb.ARVALID;
+    t.ARPROT  = vif.inp_mon_cb.ARPROT;
+      
     t.RREADY  = vif.inp_mon_cb.RREADY;
-    
-   // t.AWPROT  = vif.inp_mon_cb.AWPROT;
-   // t.ARPROT  = vif.inp_mon_cb.ARPROT;
-    
+
     inp_mon_port.write(t);
+
+
   endtask
   
 endclass
