188990fbd130afc891375d22a6d1b07903e5985a
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:33:53 +0530

    updated

--- src/TB/inp_drv.sv
+++ src/TB/inp_drv.sv
@@ -4,7 +4,7 @@ class inp_drv extends uvm_driver#(trans);
   
   axi4_cfg c_h;
   virtual axi4_if.IN_DRV vif;
-  trans t;
+  trans req;
 
   function new(string name="inp_drv",uvm_component parent);
     super.new(name,parent);
@@ -22,62 +22,71 @@ class inp_drv extends uvm_driver#(trans);
   endfunction
   
   task run_phase(uvm_phase phase);
-    
     @(vif.inp_drv_cb);
     
     forever begin
       seq_item_port.get_next_item(req);
       drive(req);
-      // Hold this transaction for one clock
-      @(vif.inp_drv_cb);
-      `uvm_info("inp_drv",$sformatf("input driver:\n%s",req.sprint()),UVM_HIGH);
-      seq_item_port.item_done();
+      seq_item_port.item_done(req);
     end
     
   endtask
   
-  task drive(trans t);
-    
-    
-     @(vif.inp_drv_cb);
-    
-    if(t.AWVALID || t.WVALID)begin
-      
-      if(t.AWVALID)begin //&& (!a_flag)
-        vif.inp_drv_cb.AWADDR  <= t.AWADDR;
-        vif.inp_drv_cb.AWVALID <= t.AWVALID;
-        vif.inp_drv_cb.AWPROT <= t.AWPROT;
-        //a_flag=1'b1;
-      end
-      
-      if(t.WVALID) begin //&& (!d_flag)
-        vif.inp_drv_cb.WDATA   <= t.WDATA;
-        vif.inp_drv_cb.WSTRB   <= t.WSTRB;
-        vif.inp_drv_cb.WVALID  <= t.WVALID;
-        //d_flag=1'b1;
-      end
-      
-      if(t.BREADY && vif.inp_drv_cb.BVALID)begin
-        vif.inp_drv_cb.BREADY  <= t.BREADY;
-        //a_flag=1'b0;
-        //d_flag=1'b0;
-      end
-        
-    end
-    if (t.ARVALID) begin
-      vif.inp_drv_cb.ARADDR  <= t.ARADDR;
-      vif.inp_drv_cb.ARVALID <= t.ARVALID;
-      vif.inp_drv_cb.ARPROT <= t.ARPROT;
-      
-      while(!(t.RREADY && vif.inp_drv_cb.RVALID))begin
-        @(vif.inp_drv_cb);
-      end
-      
-      vif.inp_drv_cb.RREADY  <= t.RREADY;
-    end
-    
-    
+  task drive(trans req);
+    @(vif.inp_drv_cb);
+    fork 
+      write_addr(req);
+      write_data(req);
+      read_addr(req);
+      write_resp(req);
+      read_resp(req);
+      slave_resp(req);
+    join
+  endtask
+   
+  task write_addr(trans t);
+    vif.inp_drv_cb.AWADDR  <= t.AWADDR;
+    vif.inp_drv_cb.AWVALID <= t.AWVALID;
+    vif.inp_drv_cb.AWPROT <= t.AWPROT;
   endtask
   
+  task write_data(trans t);
+    vif.inp_drv_cb.WDATA   <= t.WDATA;
+    vif.inp_drv_cb.WSTRB   <= t.WSTRB;
+    vif.inp_drv_cb.WVALID  <= t.WVALID;
+  endtask
+  
+  task read_addr(trans t);
+    vif.inp_drv_cb.ARADDR  <= t.ARADDR;
+    vif.inp_drv_cb.ARVALID <= t.ARVALID;
+    vif.inp_drv_cb.ARPROT <= t.ARPROT; 
+  endtask
+
+  task write_resp(trans t);
+    vif.inp_drv_cb.BREADY  <= t.BREADY;
+  endtask
+
+  task read_resp(trans t);
+    vif.inp_drv_cb.RREADY  <= t.RREADY;
+  endtask
+
+  task slave_resp(trans t);
+   // $cast(res,t.clone());
+    
+    t.AWREADY = vif.inp_drv_cb.AWREADY;
+    
+    t.WREADY  = vif.inp_drv_cb.WREADY;
+  
+    t.BVALID  = vif.inp_drv_cb.BVALID;
+    t.BRESP   = vif.inp_drv_cb.BRESP;
+
+    t.ARREADY = vif.inp_drv_cb.ARREADY;
+
+    t.RRESP  = vif.inp_drv_cb.RRESP;
+    t.RDATA  = vif.inp_drv_cb.RDATA;
+    t.RVALID = vif.inp_drv_cb.RVALID;
+
+  endtask
+
 endclass
     
