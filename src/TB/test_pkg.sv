14398c208dcbe3caa0dd656dcccd4169a0221d64
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 09:38:08 +0530

    updated

--- src/TB/test_pkg.sv
+++ src/TB/test_pkg.sv
@@ -1,24 +1,25 @@
 package test_pkg;
+
 import uvm_pkg::*;
-`include "uvm_macros.svh"
 `include "defines.svh"
-  
-`include "alu_cfg.sv"
-  
+`include "uvm_macros.svh"
+
+`include "axi4_cfg.sv"
 `include "seq_item.sv"
+
 `include "sequence.sv"
 `include "sequencer.sv"
-  
+
 `include "inp_drv.sv"
 `include "inp_mon.sv"
 `include "out_mon.sv"
-  
+
 `include "inp_agnt.sv"
 `include "out_agnt.sv"
-  
+
 `include "scoreboard.sv"
 `include "subscriber.sv"
-  
 `include "env.sv"
 `include "test.sv"
-endpackage 
+
+endpackage
