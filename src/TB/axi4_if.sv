11292534de0322696cc70e063b323b704e7eb725
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-09-21 13:26:19 +0530

    updated

--- src/TB/axi4_if.sv
+++ src/TB/axi4_if.sv
@@ -28,20 +28,6 @@ interface axi4_if(input logic ACLK,input logic ARESETn);
   clocking inp_drv_cb@(posedge ACLK);
     default input #1 output #1;
 
-    input AWREADY;
-
-    input WREADY;
- 
-    
-    input BRESP;
-    input BVALID;
-
-    input ARREADY;
-    
-    input RRESP;
-    input RDATA;
-    input RVALID;
-
     output AWADDR;
     output AWPROT;
     output AWVALID;
@@ -58,6 +44,7 @@ interface axi4_if(input logic ACLK,input logic ARESETn);
     output ARVALID;
     
     output RREADY;
+
   endclocking
   
   
@@ -103,99 +90,13 @@ interface axi4_if(input logic ACLK,input logic ARESETn);
   modport IN_DRV(clocking inp_drv_cb);
   modport IN_MON(clocking inp_mon_cb);
   modport OUT_MON(clocking out_mon_cb);
-/*
-   // FSM signals coming from DUT
-  logic [2:0] wr_state;
-  logic       rd_state;
-    
-  localparam [2:0]
-      w_idle = 3'd0,
-      w_both = 3'd1,
-      w_addr = 3'd2,
-      w_data = 3'd3,
-      w_resp = 3'd4;
 
-  localparam
-      r_idle = 1'd0,
-      r_data = 1'd1;
-    
-  check_rst:assert property(
-    @(posedge ACLK)
-    !ARESETn|->(!AWREADY && !WREADY && !BVALID && !ARREADY && !RVALID)
-    )
-    else
-      `uvm_error("assertion_fail","reset failed"); 
-    
-     
-  widle_wboth:assert property(
-     @(posedge ACLK)
-     (w_state==w_idle && ARESETn)|=> (w_state==w_both)
-    )
-    else
-      `uvm_error("assertion_fail","w_idle->w_both");
-     
-   wboth_wresp:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     ((w_state==w_both) && AWVALID && WVALID)|=> (w_state==w_resp)
-    )
-    else
-      `uvm_error("assertion_fail","w_both->w_resp");
-     
-   wboth_waddr:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     ((w_state==w_both) && AWVALID && !WVALID)|=> (w_state==w_addr)
-    )
-    else
-      `uvm_error("assertion_fail","w_both->w_addr");
-     
-   wboth_wdata:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     ((!AWVALID && WVALID) && (w_state==w_both))|=> (w_state==w_data)
-    )
-    else
-      `uvm_error("assertion_fail","w_both->w_data");
-     
-   waddr_wresp:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     (WVALID  && WREADY && (w_state==w_addr)) |=> (w_state==w_resp)
-    )
-    else
-      `uvm_error("assertion_fail","w_addr->w_resp");
-     
-   wdata_wresp:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     (AWVALID  && AWREADY && (w_state==w_data) ) |=> (w_state==w_resp)
-    )
-    else
-      `uvm_error("assertion_fail","w_data->w_resp");
-    
-   wresp_widle:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     ((w_state==w_resp) && BVALID && BREADY) |=> (w_state==w_idle)
-    )
-    else
-      `uvm_error("assertion_fail","w_resp->w_idle");
-    
-   ridle_rdata:assert property(
-     @(posedge ACLK)
-     (r_state==r_idle && ARESETn) |=> (r_state==r_data)
-    )
-    else
-      `uvm_error("assertion_fail","r_idle->r_data");
-   
-   rdata_ridle:assert property(
-     @(posedge ACLK)
-     disable iff(!ARESETn)
-     (r_state==r_data && RVALID && RREADY) |=> (r_state==r_idle)
-    )
-    else
-      `uvm_error("assertion_fail","r_data->r_idle");
-      
-    */
+  syn_rst:assert property (@(posedge ACLK) !ARESETn |-> ({AWREADY, WREADY, BVALID, BRESP, ARREADY, RVALID, RDATA, RRESP} == 0));
+  asyn_rst:assert property (@(negedge ACLK) !ARESETn |-> ({AWREADY, WREADY, BVALID, BRESP, ARREADY, RVALID, RDATA, RRESP} == 0));
+  checkbvalid:assert property (@(posedge ACLK) disable iff (!ARESETn) (BVALID && !BREADY) |=> BVALID);
+  checkbresp:assert property (@(posedge ACLK) disable iff (!ARESETn) (BVALID && !BREADY) |=> ($stable(BRESP)));
+  checkrvalid:assert property (@(posedge ACLK) disable iff (!ARESETn) (RVALID && !RREADY) |=> RVALID);
+  checkrdata:assert property (@(posedge ACLK) disable iff (!ARESETn) (RVALID && !RREADY) |=> ($stable(RDATA)));
+  checkrresp:assert property (@(posedge ACLK) disable iff (!ARESETn) (RVALID && !RREADY) |=> ($stable(RRESP)));
+
 endinterface
